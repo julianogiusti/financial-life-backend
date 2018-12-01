a6aa57a5162af8157ef611961e492fa195bf7a63
Juliano Giusti <giustijuliano@gmail.com>
2018-11-30 22:18:26 -0200

    Permitindo cors...

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -1,5 +1,8 @@
 from flask import Blueprint
+from flask_cors import CORS
 
 bp = Blueprint('api', __name__)
+
+CORS(bp, resources=r'/api/*')
 
 from app.api import users, accounts, transfers, transactions, errors, tokens
